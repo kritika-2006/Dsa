407afe0d8ee23019a403ac18cee9a0fe62bbd6dd
kritika-2006 <kritika02056@gmail.com>
2026-07-24 18:31:11 +0530

    main

---
+++ day5.py/array.py/pivot.py
@@ -0,0 +1,13 @@
+def pivotindex(nums):
+    total_sum = sum(nums)
+    left_sum = 0
+
+    for i in range(len(nums)):
+        right_sum = total_sum - left_sum - nums[i]
+        if left_sum == right_sum:
+            return i
+        # left sum ko update 
+        left_sum += nums[i]
+    return -1
+nums = [1,7,3,6,5,6]
+print("Pivot index is:",pivotindex(nums))
