64f4c0c83d370670e5b3cec6bbef128f697b612f
kritika-2006 <kritika02056@gmail.com>
2026-07-28 19:37:29 +0530

    main

---
+++ day5.py/array.py/dup.py
@@ -0,0 +1,10 @@
+def remove_duplicates(nums):
+    slow = 0
+    for fast in range(1,len(nums)):
+        if nums[fast] != nums[slow]:
+            slow += 1
+            nums[slow] = nums[fast]
+        
+    return slow + 1
+nums = [1, 1, 2,2,3]
+print("new array:",remove_duplicates(nums))
