059fbc35e121ac0ef7daa6d78802b1077d7cf541
kritika-2006 <kritika02056@gmail.com>
2026-07-29 19:24:01 +0530

    main

--- day5.py/array.py/sliding.py
+++ day5.py/array.py/sliding.py
@@ -1,11 +1,16 @@
-def sliding_window(nums,k):
+def maxaverage_subarray(nums,k):
     current_sum = sum(nums[:k])
     max_sum = current_sum
+
     for i in range (k,len(nums)):
+        # new element add on 
         current_sum += nums[i]
+        # purana element remove
         current_sum -= nums[i-k]
-        max_sum = max(max_sum, current_sum)
+        # update max_sum
+        max_sum = max(max_sum,current_sum)
     return max_sum / k 
-nums = [1,12,-5,-6,50,3]
+nums = [1, 12, -5, -6, 50, 3]
 k = 4
-print("Average of max:",sliding_window(nums,k))
+print("max average of the sum :",maxaverage_subarray(nums,k))
+         
---
+++ day5.py/array.py/test.py
@@ -0,0 +1,10 @@
+def move_zeros(nums):
+    slow = 0
+    for fast in range(len(nums)):
+        if nums[fast] != 0:
+            nums[fast],nums[slow] = nums[slow],nums[fast]
+            slow += 1
+    
+nums = [0, 1, 0, 3, 12]
+move_zeros(nums)
+print("updated array:",nums)
