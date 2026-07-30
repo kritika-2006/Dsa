e1962e65686f2c9df2068db0d6ed2ff96d52ce06
kritika-2006 <kritika02056@gmail.com>
2026-07-30 19:18:35 +0530

    main

--- day5.py/array.py/var sliding window.py
+++ day5.py/array.py/var sliding window.py
@@ -1,20 +1,17 @@
 def min_subarray_len(target, nums):
     left = 0
     current_sum = 0
-    min_length = 999999
-    
+    min_length = float('inf')
+
     for right in range(len(nums)):
         current_sum += nums[right]
-        
-        while current_sum >= target:
-            current_length = right - left + 1
-            
-            if current_length < min_length:
-                min_length = current_length
-            
-            current_sum -= nums[left]
-            left += 1  # <-- YEH WALA ADD KARO (Line 18)
-            
-    return min_length  # <-- YEH FOR LOOP KE BAHAR HOGA
 
-print(min_subarray_len(7, [2, 3, 1, 2, 4, 3]))
+        while current_sum >= target:
+            # window size = (right - left + 1)
+            min_length = min(min_length, right - left + 1)
+            current_sum -= nums[left]
+            left += 1
+    return min_length if min_length!= float('inf') else 0
+nums = [2, 3, 1, 2, 4, 3]
+target = 7
+print("min len:",min_subarray_len(target,nums))
