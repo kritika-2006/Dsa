2b0c343433558056e6eeb51a44ac3378ec4814fa
kritika-2006 <kritika02056@gmail.com>
2026-08-02 19:53:52 +0530

    main

---
+++ day5.py/array.py/max_water.py
@@ -0,0 +1,18 @@
+def max_area(height):
+    left = 0
+    right = len(height) - 1
+    max_water = 0
+
+    while left < right:
+        width = right - left 
+        current_height = min(height[left],height[right]) 
+        max_water = max(max_water, width * current_height)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_water
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print("min height:",max_area(height))
