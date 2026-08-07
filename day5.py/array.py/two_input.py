2c0ed2d3c94fd70f4bfa8da94006ab573d392c6c
kritika-2006 <kritika02056@gmail.com>
2026-08-07 10:59:54 +0530

    main

--- day5.py/array.py/two_input.py
+++ day5.py/array.py/two_input.py
@@ -1,19 +1,9 @@
-def two_input(nums,target):
-    left = 0
-    right = len(nums) - 1
-# two pointer vala ma always while loop 
-    while left < right:
-        current_sum = nums[left] + nums[right]
-        if current_sum == target:
-            return [left + 1, right + 1]
-        elif current_sum > target:
-            right -= 1
-        elif current_sum < target:
-            left += 1
-    
-nums = [2,7,11,15]
-target = 9
-print("sum:",two_input(nums,target))
-
-        
-
+def two_sum(nums,target):
+    n = len(nums)
+    for i in range(0,n-1):
+        for j in range(i+1,n):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+nums = [5,9,1,2,4,15,6,3]
+target = 13
+print("index of the sum:",two_sum(nums,target))
