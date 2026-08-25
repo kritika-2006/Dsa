1efdb0a9af0cdbd401341b5653826696fc2bb69c
kritika <kritika02056@gmail.com>
2026-08-25 19:32:18 +0530

    Time: 4 ms (6.7%), Space: 19.3 MB (53.35%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -3,8 +3,8 @@ class Solution:
         
         num_set = set(nums)
         multiple = k
-         
-        while multiple in num_set: 
+        
+        while multiple in num_set:
             multiple += k
             
         return multiple
